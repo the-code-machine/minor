ecf928d2b1676bdbe904a466f3142d39cf9ccb05
the-code-machine <sarthak25ic049@satiengg.in>
2024-05-23 19:16:23 +0530

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "DRS",
-  description: "Automatic Data Recovery System",
+  description: "Automatic Detection Remote System",
 };
 
 export default function RootLayout({ children }) {
